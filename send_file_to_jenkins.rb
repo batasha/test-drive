b617c5984d29d6bb94cb4cca6ec1e7cfa6e40612
Natasha Badillo <nbadillo@amplify.com>
2015-02-06 15:12:07 -0500

    CTI-31 kickoff TestDrive job in Jenkins and output result to terminal

--- send_file_to_jenkins.rb
+++ send_file_to_jenkins.rb
@@ -13,24 +13,27 @@ id_param = {'name' => 'TRACKING_ID', 'value' => tracking_id}
 build = 0
 start = 0
 
-res = `curl -i ***REMOVED***/job/PBPatchTest/build -F file0=@patch -F json='{"parameter": [{"name":"patch", "file":"file0"}, {"name":"TRACKING_ID", "value":"#{tracking_id}"}]}' --user '***REMOVED***':'***REMOVED***'`
+res = `curl -i ***REMOVED***/job/***REMOVED***/build -F file0=@patch -F json='{"parameter": [{"name":"patch", "file":"file0"}, {"name":"TRACKING_ID", "value":"#{tracking_id}"}]}' --user '***REMOVED***':'***REMOVED***'`
 
 
 12.times do
   sleep 10
-
-  if @client.job.get_build_details('PBPatchTest', 0)['actions'][0]['parameters'].include?(id_param)
-    build = @client.job.get_current_build_number 'PBPatchTest'
+  if @client.job.get_build_details('***REMOVED***', 0)['actions'][0]['parameters'].include?(id_param)
+    build = @client.job.get_current_build_number '***REMOVED***'
     break
   end
 end
 
-job_output = @client.job.get_console_output('PBPatchTest', build, start)
-
 loop do
+  job_output = @client.job.get_console_output('***REMOVED***', build, start)
+
   print job_output['output']
   break unless job_output['more']
 
   start = job_output['size']
-  job_output = @client.job.get_console_output('PBPatchTest', build, start)
 end
+
+result = @client.job.get_build_details('***REMOVED***', build)['result']
+
+# `git push origin master` if result == 'SUCCESS' || result == 'UNSTABLE'
+`rm patch`
